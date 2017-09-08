e054b4c4bb8bb0fd77b6c898c5a60953c6cd6279
scannella <scannella@bishopireton.org>
2017-09-08 12:28:12 -0400

    Initial Commit

---
+++ src/Quadratic.java
@@ -0,0 +1,17 @@
+/**
+ * Aidan Scannell
+ * Duel Enrollment
+ */
+
+
+public class Quadratic {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
