ff943455eb76287a9e90d49277d83c22ec3d078f
scannella <scannella@bishopireton.org>
2017-09-08 12:43:23 -0400

    New Code

--- src/Quadratic.java
+++ src/Quadratic.java
@@ -6,12 +6,38 @@
 
 public class Quadratic {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+private double a;
+private double b;
+private double c;
 
+	
+
+	Quadratic(double a, double b, double c){
+		this.a = a;
+		this.b = b;
+		this.c = c;
 	}
-
+	
+	public double discriminant(){
+		return Math.pow(b, 2)-4*a*c;
+		
+	/*	try {
+			d = Mat
+		}catch {ArithmaticException e}{
+			
+		}*/
+		
+	}
+	
+	public double[] roots() {
+		double[] allroots;
+		double d = discriminant();
+		
+		if (d < 0) return null;
+		if (d == 0) {
+			allroots = new double [1];
+			all
+		}
+	}
+	
 }
